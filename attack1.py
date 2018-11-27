d37931cb3c09e2c50667eac7b1f75a65bcce9104
Charles Parbax <charles@MacBook-Pro-de-Charles.local>
2018-11-27 20:50:21 +0100

    top produit par user

--- attack1.py
+++ attack1.py
@@ -1,26 +1,24 @@
 import pandas as pd
+from collections import OrderedDict
 
 
 T_COL = {'id_user': 0, 'date': 1, 'hours': 2, 'id_item': 3, 'price': 4, 'qty': 5}
 users_items = {}
 
 
-
 def search_id(row):
 
-    id_row = row[T_COL['id_user']]
+    id_user = row[T_COL['id_user']]
     id_product = row[T_COL['id_item']]
     qty = row[T_COL['qty']]
-    if id_row in users_items:
-        if id_product in users_items[id_row]:
-            users_items[id_row][id_product] += qty
-        else :
-            users_items[id_row] = pd{
-                id_product: [qty]
-            }
+    if id_user in users_items:
+        if id_product in users_items[id_user]:
+            users_items[id_user][id_product] += qty
+        else:
+            users_items[id_user][id_product] = qty
     else:
-        users_items[id_row] = pd{
-            id_product: [qty]
+        users_items[id_user] = {
+            id_product: qty
         }
 
 
@@ -35,11 +33,16 @@ def main():
     df = pd.read_csv(file_path, sep=',', engine='c', na_filter=False, low_memory=False)
 
     df.apply(search_id, axis=1)
-   # df_user = pd.DataFrame.from_dict(users_items)
-    print(users_items)
+
+    users_items_sorted = {}
+    for user in users_items.items():
+        users_items_sorted[user[0]] = (sorted(user[1].items(), key=lambda t: t[1], reverse=True))
+
+    print(users_items_sorted)
 
     print("All clear")
 
 
 if __name__ == "__main__":
         main()
+
