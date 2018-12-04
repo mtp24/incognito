8aa3bfca197a9ae90f7cbb46467a6747a50f2688
Ros <rosbenjamindu01@gmail.com>
2018-12-04 19:27:49 +0100

    function without globals objects

--- attack1.py
+++ attack1.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import random
+import csv
 from collections import OrderedDict
 
 
 T_COL = {'id_user': 0, 'date': 1, 'hours': 2, 'id_item': 3, 'price': 4, 'qty': 5}
-users_items = {}
-users_items_sorted = {}
-unique_top_user = []
-unique_users_random_items = []
-users_list_items = {}
 
-def search_id(row, u_items):
+unique_top_user = []
+
+
+def search_id(row, u_items, u_list_items):
 
     id_user = row[T_COL['id_user']]
     id_product = row[T_COL['id_item']]
@@ -22,12 +21,12 @@ def search_id(row, u_items):
             u_items[id_user][id_product] += qty
         else:
             u_items[id_user][id_product] = qty
-            users_list_items[id_user].append(id_product)
+            u_list_items[id_user].append(id_product)
     else:
         u_items[id_user] = {
             id_product: qty
         }
-        users_list_items[id_user] = [id_product]
+        u_list_items[id_user] = [id_product]
 
 def sort_items(u_items, u_items_sorted):
     for user in u_items.items():
@@ -82,9 +81,32 @@ def find_uniqueness_random_items(nb_repetition, nb_items, u_list_items, unique_u
                         uniqueness = False
                         break
 
-            if uniqueness == True and {user : items_user} not in unique_u_random_items:
-                unique_u_random_items.append({user: items_user})
+            if uniqueness == True and user not in unique_u_random_items:
+                unique_u_random_items[user] = items_user
                 break
+
+
+def file_analysis(path, u_items,u_list_items, u_items_sorted):
+
+
+    df = pd.read_csv(path, sep=',', engine='c', na_filter=False, low_memory=False)
+    df.apply(lambda row: search_id(row, u_items, u_list_items), axis=1)
+    sort_items(u_items, u_items_sorted)
+
+
+def find_match_random(unique_users_random_items_anonymized, users_list_items_truth, id_matching):
+    for anonymized_user in unique_users_random_items_anonymized:
+        count_match = 0
+        for user in users_list_items_truth:
+            if set(unique_users_random_items_anonymized[anonymized_user]).issubset(users_list_items_truth[user]):
+                count_match += 1
+                same_user = user
+
+        if count_match == 1:
+            id_matching[same_user] = anonymized_user
+
+
+
 
 
 
@@ -93,28 +115,49 @@ def main():
     """main
     """
     print("Beginning of the script")
+    path_truth = "./data/ground_truth.csv"
 
-    file_path = "./data/ground_truth.csv"
-  #  global users_items
-#    global users_items_sorted
+    users_items_truth = {}
+    users_list_items_truth = {}
+    users_items_sorted_truth = {}
+   # unique_users_random_items_truth = []
 
-    df = pd.read_csv(file_path, sep=',', engine='c', na_filter=False, low_memory=False)
+    file_analysis(path_truth, users_items_truth, users_list_items_truth,  users_items_sorted_truth)
 
-    df.apply(lambda row: search_id(row, users_items), axis=1)
-    sort_items(users_items, users_items_sorted)
-    nbUser = len(users_items_sorted)
-    print ("nbUser", nbUser)
+    nbUser_truth = len(users_items_sorted_truth)
+    print("nbUser truth", nbUser_truth)
+    path_analysis = "./data/ouputTestprice.csv"
+
+    users_items_analysis = {}
+    users_list_items_analysis = {}
+    users_items_sorted_analysis = {}
+    unique_users_random_items_anonymized = {}
+
+    file_analysis(path_analysis, users_items_analysis, users_list_items_analysis, users_items_sorted_analysis)
+    find_uniqueness_random_items(2, 4, users_list_items_analysis, unique_users_random_items_anonymized)
+
+    print("NB anonymized users potentially identifiable", len(unique_users_random_items_anonymized))
+    id_matching = {}
+    find_match_random(unique_users_random_items_anonymized, users_list_items_truth, id_matching)
+    #print(id_matching)
+
+    print("Nb identified", len(id_matching))
+
+    with open('dict.csv', 'wb') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in unique_users_random_items_anonymized.items():
+            print("key", key)
+            print("value", value)
+            writer.writerow([key, value])
 
 
-   # for i in range (1,6):
+    # for i in range (1,6):
     #    find_uniqueness_top_items(i)
      #   print("Nb unique d'user: ", len(unique_top_user), ", pour", i, "articles")
 
-#    print(users_items_sorted)
-   # print(users_list_items)
-    unique_users_random_items = []
-    find_uniqueness_random_items(5, 6, users_list_items, unique_users_random_items)
-    print(len(unique_users_random_items))
+
+
+
 
 
     print("All clear")
